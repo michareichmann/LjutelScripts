cae48c21b3f12493a1dd21b6bafba7803424c4fc
michareichmann <micha.reichmann@gmail.com>
2018-10-14 12:48:40 +0200

    fix say script

--- say.py
+++ say.py
@@ -9,13 +9,9 @@ from argparse import ArgumentParser
 from gtts import gTTS
 from os import devnull
 from random import randint
+from os.path import dirname, realpath, join
 
 FNULL = open(devnull, 'w')
-
-f = open('haikus.txt')
-
-lines = [line.strip('\n,') for line in f.readlines() if line != '\n']
-haikus = [', '.join(lines[i:i+3]) for i in xrange(0, len(lines), 3)]
 
 
 parser = ArgumentParser()
@@ -24,11 +20,15 @@ parser.add_argument('-l', nargs='?', default='en')
 
 args = parser.parse_args()
 
+d = dirname(realpath(__file__))
+f = open(join(d, 'haikus.txt'))
+lines = [line.strip('\n,') for line in f.readlines() if line != '\n']
+haikus = [', '.join(lines[i:i + 3]) for i in xrange(0, len(lines), 3)]
 
-def say(txt):
-    tts = gTTS(text=txt, lang=args.l)
+def say():
+    txt = args.txt if args.txt else haikus[randint(0, len(haikus) - 1)]
+    tts = gTTS(text=txt.decode('utf-8'), lang=args.l)
     tts.save('good.mp3')
     call(['mpg321', 'good.mp3'], stdout=FNULL)
 
-
-say(haikus[randint(0, len(haikus) - 1)].decode('utf-8') if not args.txt else args.txt.decode('utf-8'))
+say()
